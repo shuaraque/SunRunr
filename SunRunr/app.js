37e5be1d88c16082eab372007aacb428278bb416
shuaraque <56464580+shuaraque@users.noreply.github.com>
2019-11-19 18:08:32 -0700

    Update app.js

--- SunRunr/app.js
+++ SunRunr/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
+var photonRouter = require('./routes/activity');
 var usersRouter = require('./routes/users');
+var devicesRouter = require('./routes/devices');
 
 var app = express();
 
@@ -13,14 +15,33 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// This is to enable cross-origin access
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
+app.use('/devices', devicesRouter);
+app.use('/photon', photonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
