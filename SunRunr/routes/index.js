56e1487b6d270efc754f77ea311fe49634afb3e8
jcogden <44734783+jcogden@users.noreply.github.com>
2019-11-19 18:47:30 -0700

    Update index.js

--- SunRunr/routes/index.js
+++ SunRunr/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/html/index.html', { title: 'Express' });
+  res.render('/html/index', { title: 'Express' });
 });
 
 module.exports = router;
