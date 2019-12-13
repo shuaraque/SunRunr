64fb10e91df2a2c889445843ea640e9a25b54555
Bader <bjeragh@email.arizona.edu>
2019-12-13 16:58:55 -0700

    models updated

--- SunRunr/models/device.js
+++ SunRunr/models/device.js
@@ -1,12 +1,10 @@
 var db = require("../db");
-
 var deviceSchema = new db.Schema({
-    apikey:       String, // not sure what this is used for
-    email:        String,
-    deviceID:     String,
-    // lastContact:  { type: Date, default: Date.now }
+	apikey: String,
+	email: String,
+	deviceID:String,
+	lastContact:  { type: Date, default: Date.now }
 });
 
 var Device = db.model("Device", deviceSchema);
-
 module.exports = Device;
--- SunRunr/models/users.js
+++ SunRunr/models/users.js
@@ -1,13 +1,16 @@
 var db = require("../db");
 
 var userSchema = new db.Schema({
-  email:        { type: String, required: true, unique: true },
-  name:         { type: String, required: true },
-  hashedPassword: String,
-  // lastAccess:   { type: Date, default: Date.now },
-  devices:      [ String ],
+	email:	{ type: String, required: true, unique: true },
+	name:	{ type: String, required: true },
+	longitude:	{ type: Number, default: -110 },
+	latitude:	{ type: Number, default: 32 },
+	city:	{ type: String, default: "Tucson, Arizona, United States of America" },  
+	hashedPassword: String,
+	lastAccess:   { type: Date, default: Date.now },
+	devices:      [ String ],
+	uvThreshold:  { type: Number, default: 10 },
 });
 
 var User = db.model("User", userSchema);
-
 module.exports = User;
