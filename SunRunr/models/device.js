f0f2bff6ad8405907dc4e5deb231f7c5e410ce02
Bader <bjeragh@email.arizona.edu>
2019-12-14 16:26:44 -0700

    fixed models/device.js

--- SunRunr/models/device.js
+++ SunRunr/models/device.js
@@ -1,16 +1,9 @@
 var db = require("../db");
 var deviceSchema = new db.Schema({
-<<<<<<< HEAD
-    apikey:       String, // not sure what this is used for
-    email:        String,
-    deviceID:     String,
-    lastContact:  { type: Date, default: Date.now }
-=======
 	apikey: String,
 	email: String,
-	deviceID:String,
-	lastContact:  { type: Date, default: Date.now }
->>>>>>> 01ee1e7bb262b55d4a3d7200af12bd8947c5e1ad
+	deviceID: String,
+	lastContact: { type: Date, default: Date.now }
 });
 
 var Device = db.model("Device", deviceSchema);
