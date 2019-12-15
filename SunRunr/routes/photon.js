38ac8b2ceb42c89ecc1629d60f117d3f0990371b
Bader <bjeragh@email.arizona.edu>
2019-12-14 22:49:59 -0700

    started the big routes/photon.js file

--- SunRunr/routes/photon.js
+++ SunRunr/routes/photon.js
@@ -9,4 +9,39 @@ let jwt = require("jwt-simple");
 var secret = fs.readFileSync(__dirname + '/../jwtkey.txt').toString();
 
 
+// pre: an array called activityArray that contains activities, which then contain speed 
+// post: returns the average speed in the activityArray
+function calculateAverageSpeed(activityArray) {
+    let averageSpeed = 0.0;
+    let type = "";
+
+    for(activity of activityArray) {
+        averageSpeed += parseFloat(activity.speed);
+    }
+    averageSpeed /= activityArray.length;
+
+    if(speed <= 5) {
+        type = "walking";
+    } else if (speed > 5 && speed <= 9) {
+        type = "running";
+    } else if (speed > 9){
+        type = "biking";
+    }
+    return {"averageSpeed": averageSpeed, "type": type};
+}
+
+// pre: an activityArray that contains activities
+// post: returns the average ultraviolet index in the activityArrays
+function calculateUV(activityArray){
+    let averageUV = 0.0;
+    for(activity of activityArray) {
+        averageUV += activity.ultraviolet;
+    }
+    averageUV /= activity.length;
+    return averageUV;
+}
+
+router.post('/getThreshold', (req, res)=> {
+    User.findOne();
+});
 module.exports = router;
