5db13d06d61d251da9f823331dc50309638a1857
shuaraque <56464580+shuaraque@users.noreply.github.com>
2019-12-16 12:36:55 -0700

    Update activity.js

--- SunRunr/public/javascript/activity.js
+++ SunRunr/public/javascript/activity.js
@@ -1,1 +1,56 @@
+function sendReqForActivityInfo() {
+  $.ajax({
+    url: '/users/activities',
+    type: 'GET',
+    headers: { 'x-auth': window.localStorage.getItem("authToken") },
+    dataType: 'json'
+  })
+    .done(activitiesInfoSuccess)
+    .fail(activitiesInfoError);
+}
 
+function activitiesInfoSuccess(data, textSatus, jqXHR) {
+  $("#email").html(data.email);
+  $("#fullName").html(data.name);
+  $("#lastAccess").html(data.lastAccess);
+  $("#main").show();
+  $("#UVDisplay").html(data.uvThreshold);
+  
+  // Add the devices to the list before the list item for the add device button (link)
+  for (var device of data.devices) {
+    $("#addDeviceForm").before("<li class='collection-item'>ID: " +
+      device.deviceId + ", APIKEY: " + device.apikey + 
+      " <button id='ping-" + device.deviceId + "' class='waves-effect waves-light btn'>Ping</button> " +
+      " </li>");
+    $("#ping-"+device.deviceId).click(function(event) {
+      pingDevice(event, device.deviceId);
+    });
+  }
+}
+
+function activitiesInfoError(jqXHR, textStatus, errorThrown) {
+  // If authentication error, delete the authToken 
+  // redirect user to sign-in page (which is index.html)
+  if( jqXHR.status === 401 ) {
+    window.localStorage.removeItem("authToken");
+    window.location.replace("index.html");
+  } 
+  else {
+    $("#error").html("Error: " + status.message);
+    $("#error").show();
+  } 
+}
+
+// Handle authentication on page load
+$(function() {
+  // If there's no authToekn stored, redirect user to 
+  // the sign-in page (which is index.html)
+  if (!window.localStorage.getItem("authToken")) {
+    window.location.replace("index.html");
+  }
+  else {
+    sendReqForActivityInfo();
+  }
+  
+  // Register event listeners
+});
