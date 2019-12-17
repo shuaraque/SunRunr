371268011b0bdc0cebfac92911105ebebdfcc493
shuaraque <56464580+shuaraque@users.noreply.github.com>
2019-12-16 23:24:06 -0700

    Update activity.js

--- SunRunr/public/javascript/activity.js
+++ SunRunr/public/javascript/activity.js
@@ -10,7 +10,7 @@ function sendReqForDeviceInfo() {
 }
 
 function activitiesInfoSuccess(data, textSatus, jqXHR) {
-console.log(data.devices[0].deviceID);
+console.log(data[0].deviceID);
 }
 
 function activitiesInfoError(jqXHR, textStatus, errorThrown) {
