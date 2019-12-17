6e16d78de52a91ffaa5e7af4b65efc5b3c0c49f4
shuaraque <56464580+shuaraque@users.noreply.github.com>
2019-12-17 15:51:45 -0700

    Update device.js

--- SunRunr/models/device.js
+++ SunRunr/models/device.js
@@ -3,7 +3,8 @@ var deviceSchema = new db.Schema({
 	apikey: String,
 	email: String,
 	deviceID: String,
-	lastContact: { type: Date, default: Date.now }
+	lastContact: { type: Date, default: Date.now },
+	activities : [ String ],
 });
 
 var Device = db.model("Device", deviceSchema);
