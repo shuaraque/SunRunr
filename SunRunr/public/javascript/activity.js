cec3eba801ab6ee34ac7bc45a813292cb3d34543
shuaraque <56464580+shuaraque@users.noreply.github.com>
2019-12-17 23:18:42 -0700

    Update activity.js

--- SunRunr/public/javascript/activity.js
+++ SunRunr/public/javascript/activity.js
@@ -19,8 +19,60 @@ function devicesInfoError(jqXHR, textStatus, errorThrown) {
     $("#error").html("Error: " + status.message);
     $("#error").show();
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function sendReqForSummaryInfo(){  //done SUMMARY
+    $.ajax({
+    url: '/users/activities',
+    type: 'GET',
+    headers: { 'x-auth': window.localStorage.getItem("authToken") },
+    data : { deviceID : $("#deviceInput2").val()},
+    dataType: 'json'
+  })
+    .done(SummarySuccess)
+    .fail(SummaryError);
+}
 
-function sendReqForActivityInfo(){
+function SummarySuccess(data, textSatus, jqXHR) {
+  $("#deviceInputPage").hide();
+  let totalTime = 0;
+  let totalCalories = 0;
+  let totalUVExposure = 0;
+  
+  for (var activity of data.activities) {
+  let time = (activity.endTime - activity.beginTime)/(60000);
+  let calories = 0;
+    
+  if(activity.type == "walking"){
+    calories = 250 * (time/60);
+  }
+    
+  if(activity.type == "running"){
+    calories = 700 * (time/60);
+  }
+    
+  if(activity.type == "biking"){
+    calories = 572 * (time/60);
+  }
+  
+  totalCalories = calories + totalCalories;
+  totalTime = time + totalTime;
+  totalUVExposure = activity.UVSum + totalUVExposure;
+    
+
+}
+  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>Total Activity Duration: " +  totalTime + " minutes</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>Total UV Exposure: " + totalUVExposure + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>Total Calories Burned: " + totalCalories + "</li>");
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function SummaryError(jqXHR, textStatus, errorThrown) {
+    $("#error").html("Error: " + status.message);
+    $("#error").show();
+}
+
+function sendReqForActivitySummaryInfo(){ //Done
     $.ajax({
     url: '/users/activities',
     type: 'GET',
@@ -50,13 +102,65 @@ function activitiesInfoSuccess(data, textSatus, jqXHR) {
     calories = 572 * (time/60);
   }
   
-    
-  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>Total Activity Duration: " + time + " minutes</li>");
-  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>Total UV Exposure: " + activity.UVSum + "</li>");
-  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>Total Calories Burned: " + calories + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='ID'><h5>" + activity.activityID + "</h5></li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='duration'>Duration of Activity: " + time + " minutes</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>UV Exposure: " + activity.UVSum + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='calories'>Calories Burned: " + calories + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='temperature'>Temperature: " + activity.temperature + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='humidity'>Humidity: " + activity.humidity + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='date'>Date: " + activity.submissionTime.type + "</li>");
 }}
 
 function activitiesInfoError(jqXHR, textStatus, errorThrown) {
+    $("#error").html("Error: " + status.message);
+    $("#error").show();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function sendReqForActivityDetailInfo(){
+    $.ajax({
+    url: '/users/activity/' + ("#ActivitySelected").val(), //TODO Write routes function to get a specific activity detailed
+    type: 'GET',
+    headers: { 'x-auth': window.localStorage.getItem("authToken") },
+    data : { deviceID : $("#deviceInput2").val()},
+    dataType: 'json'
+  })
+    .done(activitiesDetailSuccess)
+    .fail(activitiesDetailError);
+}
+
+function activitiesDetailSuccess(data, textSatus, jqXHR) {
+  let time = (activity.endTime - activity.beginTime)/(60000);
+  let calories = 0;
+    
+  if(activity.type == "walking"){
+    calories = 250 * (time/60);
+  }
+    
+  if(activity.type == "running"){
+    calories = 700 * (time/60);
+  }
+    
+  if(activity.type == "biking"){
+    calories = 572 * (time/60);
+  }
+  
+    
+  $("#activitySummaryPage").append("<ul class='collection with-header'><li class='collection-header'><h5>" + activity.activityID + "</h5></li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='duration'>Duration of Activity: " + time + " minutes</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='UVOutput'>UV Exposure: " + activity.UVSum + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='calories'>Calories Burned: " + calories + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='temperature'>Temperature: " + activity.temperature + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='humidity'>Humidity: " + activity.humidity + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='activityType'>Activity Type: " + activity.type + "</li>");
+  $("#activitySummaryPage").append("<li class='collection-item' id='date'>Date: " + activity.submissionTime.type + "</li>");
+  $("#activitySummaryPage").append("</ul>");
+  
+  //TODO Graph Stuff
+}
+
+function activitiesDetailError(jqXHR, textStatus, errorThrown) {
     $("#error").html("Error: " + status.message);
     $("#error").show();
 }
