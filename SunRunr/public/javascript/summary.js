281ada308a7f0fcdbbc617660901073bf398268c
Bader <bjeragh@email.arizona.edu>
2019-12-18 19:16:45 -0700

    uv

--- SunRunr/public/javascript/summary.js
+++ SunRunr/public/javascript/summary.js
@@ -77,7 +77,7 @@ function weather() {
         console.log(allForcasts[j]);
         $("#f-date-" + j).html(allForcasts[j].month + " " + parseInt(parseInt(allForcasts[j].day) + j));
         //$("#f-date-" + j).html(months[allForcasts[j].month] + " " + allForcasts[j].day);
-        $("#f-temp-" + j).html(parseInt(allForcasts[j].temperature.toFixed(1) + 4.1) + "&#8451;");
+        $("#f-temp-" + j).html(parseFloat(allForcasts[j].temperature.toFixed(1) + 4.1) + "&#8451;");
       }
 
     },
